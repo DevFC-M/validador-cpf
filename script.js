23d5d38a8b56eddc3d6fd96c0679b4c4c9fe0989
Felipe Costa Developer <felipech3344@gmail.com>
2022-02-17 23:51:34 -0300

    function-validar
    Codificação da função para validar os cpf's inclusos no campo e input, formatando ja o valor incluso.

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 import ValidarCpf from "./validar-cpf.js";
 
-const cpf = document.querySelector('#cpf')
-const validarCpf = new ValidarCpf(cpf);
+const cpf = document.querySelector('#cpf');
+const validarCpf = new ValidarCpf(cpf).iniciar();
 
-console.log(validarCpf)
